64d638f9f683e3d8b7c70dd4b12ac9250ff3a7a0
Samrat <samrat2702@gmail.com>
2017-01-16 20:24:21 +0800

    Added Initial Project.

---
+++ PG_NYTimes/PG_NYTimes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PG_NYTimes
+//
+//  Created by Samrat on 16/1/17.
+//  Copyright © 2017 SMRT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
